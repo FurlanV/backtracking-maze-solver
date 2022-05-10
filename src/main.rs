00f72b2682285ba4643895fb137e0f42c7e94c06
Vinicius Roberto <vinicius.cfurlan@gmail.com>
2022-05-10 10:56:08 -0300

    Converting board values to int

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,6 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::io::Result;
 
-fn convert_to_array_board<'a>(contents: &'a str) -> Vec<Vec<Vec<&'a str>>> {
-    contents
-        .split("\n")
-        .map(|x: &str| {
-            x.split(" ")
-                .map(|x| x.split("").collect::<Vec<&str>>())
-                .collect::<Vec<Vec<&str>>>()
-        })
-        .collect::<Vec<Vec<Vec<&str>>>>()
-}
-
 fn read_file_content(filename: &str) -> Result<String> {
     let file = File::open(filename)?;
     let mut buf_reader = BufReader::new(file);
@@ -22,11 +11,39 @@ fn read_file_content(filename: &str) -> Result<String> {
     Ok(contents)
 }
 
+fn convert_board_to_array<'a>(contents: &'a str) -> Vec<Vec<&'a str>> {
+    contents
+        .split("\n")
+        .map(|x: &str| x.split(" ").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>()
+}
+
+fn convert_board_values_to_int(board: &Vec<Vec<&str>>) -> Vec<Vec<i64>> {
+    board
+        .iter()
+        .map(|x| {
+            x.iter()
+                .map(|y| y.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>()
+        })
+        .collect::<Vec<Vec<i64>>>()
+}
+
+fn print_board(board: &Vec<Vec<i64>>) {
+    for (i, row) in board.into_iter().enumerate() {
+        if i != 0 {
+            for j in row.into_iter() {
+                print!("{} ", j);
+            }
+        }
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let file_content = read_file_content("maze.txt")?;
-    let board = convert_to_array_board(&file_content);
-
-    println!("{:?}", board);
+    let board = convert_board_to_array(&file_content);
+    let parsed_board = convert_board_values_to_int(&board);
+    print_board(&parsed_board);
 
     Ok(())
 }
