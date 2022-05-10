ac573b7547f0eb0a2720b31c8646bb97e23ffaf4
Vinicius Roberto <vinicius.cfurlan@gmail.com>
2022-05-09 23:11:07 -0300

    Reading and parsing board

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,32 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::Result;
 
-fn main() -> std::io::Result<()> {
-    let file = File::open("maze.txt")?;
+fn convert_to_array_board<'a>(contents: &'a str) -> Vec<Vec<Vec<&'a str>>> {
+    contents
+        .split("\n")
+        .map(|x: &str| {
+            x.split(" ")
+                .map(|x| x.split("").collect::<Vec<&str>>())
+                .collect::<Vec<Vec<&str>>>()
+        })
+        .collect::<Vec<Vec<Vec<&str>>>>()
+}
+
+fn read_file_content(filename: &str) -> Result<String> {
+    let file = File::open(filename)?;
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
 
-    let formated_maze_values = contents
-        .split("\n")
-        .map(|x: &str| x.split(" ").collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>();
+fn main() -> std::io::Result<()> {
+    let file_content = read_file_content("maze.txt")?;
+    let board = convert_to_array_board(&file_content);
 
-    let maze = formated_maze_values.iter().map(|x| x.iter().map(|y| y.parse::<i64>().unwrap()).collect::<Vec<i64>>()).collect::<Vec<Vec<i64>>>();
-
-    for line in maze {
-        println!("{:?}", line);
-    }
-
-    // let maze_values = maze_values
-    //     .into_iter()
-    //     .map(|x: &str| match x {
-    //         "" => x.split("").collect::<Vec<&str>>(),
-    //         " " => x.split(" ").collect::<Vec<&str>>(),
-    //         _ => x.split("").collect::<Vec<&str>>(),
-    //     })
-    //     .map(|x| x.into_iter().filter(|y| y != &"").collect::<Vec<&str>>())
-    //     .collect::<Vec<Vec<&str>>>();
-
-    // for line in maze_values {
-    //     println!("{:?}", line);
-    // }
-
-    // for values in maze_values {
-    //     let current_value;
-    //     if values.contains(" ") {
-    //         current_value = values.split(" ").collect::<Vec<&str>>();
-    //     } else {
-    //         current_value = values.split("").collect::<Vec<&str>>();
-    //     }
-    //     maze.push(current_value);
-    // }
-
-    // maze = maze
-    //     .into_iter()
-    //     .map(|x| x.into_iter().filter(|x| x != &"").collect::<Vec<&str>>())
-    //     .collect::<Vec<Vec<&str>>>();
-
-    // for line in maze {
-    //     println!("{:?}", line);
-    // }
+    println!("{:?}", board);
 
     Ok(())
 }
